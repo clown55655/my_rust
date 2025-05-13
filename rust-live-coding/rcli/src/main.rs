771258924853022acb59ed543615e1ddee37705b
clown55655 <76399533+clown55655@users.noreply.github.com>
2025-05-13 20:00:05 +0800

    Add files via upload

--- rust-live-coding/rcli/src/main.rs
+++ rust-live-coding/rcli/src/main.rs
@@ -1,9 +1,28 @@
-use std::fs;
-fn main(){
-    let content = read_file("assets/juventus.csv");
-    println!("{}",content);
+use std::fs::File;
+use anyhow::Result;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Player{
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Positions")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    number: u8,
 }
 
-fn read_file(file_path: &str) -> String {
-    fs::read_to_string(file_path).expect("Error")
+fn main() -> Result<()>{
+    let file = File::open("assets/juventus.csv")?;
+    let mut reader = csv::Reader::from_reader(file);
+    for result in reader.deserialize(){
+        let player: Player = result?;
+        println!("{}", player.name);
+    }
+    Ok(())
 }
+
